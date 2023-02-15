a447f3519ca9c0b2c7390c292b83eced02896ef8
Elias-lap <eliaszerz92@gmail.com>
2023-02-15 09:24:02 +0100

    its not finished

---
+++ js/index.js
@@ -0,0 +1,61 @@
+var NameUser=document.getElementById('Name')
+var email=document.getElementById('Email')
+var password=document.getElementById('password')
+var SignUp=document.getElementById('SignUp')
+var datausers=[]
+datausers=JSON.parse(localStorage.getItem('forUsers')) 
+console.log(datausers)
+
+if (SignUp != null){
+SignUp.addEventListener('click', function(){
+  var user = {
+    nameuser:NameUser.value ,
+    emailUser:email.value ,
+    passwordUser:password.value ,
+  }
+  if (NameUser.value == ""|| email.value == "" ||password.value =="" ){
+    requiredinputs=false;
+    document.getElementById('validate').innerHTML=('All Input is required')
+    return
+  }else{
+    for(var i = 0 ; i < datausers.length ; i++){
+    if (email.value == datausers[i].emailUser){        
+      document.getElementById('validate').innerHTML=('Email is exsist')
+      return;
+   } else{
+    document.getElementById('validate').innerHTML=('success')
+   }
+
+  }
+}
+datausers.push(user)
+localStorage.setItem('forUsers' ,JSON.stringify(datausers) ) 
+clearinput()
+}
+)
+function clearinput()
+{
+  NameUser.value = ""
+  email.value = ""
+  password.value=""
+}
+}
+  
+ var logInEmail=document.getElementById('logInEmail')
+var logInPassword=document.getElementById('logInPassword')
+ var ButtonLogIn = document.getElementById('ButtonLogIn')
+var href=document.getElementById('href')
+if (ButtonLogIn!= null){
+  ButtonLogIn.addEventListener('click' , function(){
+
+    for ( var i = 0 ; i < datausers.length ; i++){
+      if (logInEmail.value.toLowerCase()==datausers[i].emailUser.toLowerCase() && logInPassword.value == datausers[i].passwordUser){
+        console.log('yes')
+       
+      }
+    }return true;
+  
+   
+  })
+
+}
